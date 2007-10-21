a0078bfccd9acff503412668d32246a0ee8ab065
Dominic Sacré <dominic.sacre@gmx.de>
2007-10-21 00:30:56 +0000

    allow single filename for -S
    git-svn-id: svn+ssh://kobol/srv/svn/klick/trunk@51 c380f6eb-e629-0410-afc2-bdd98b771d42

--- klick.cc
+++ klick.cc
@@ -205,29 +205,29 @@ void Klick::Options::print_usage(ostream & out)
 {
     out << "Usage:" << endl
         << "  klick [ options ] [meter] tempo[-tempo2/accel] [pattern]" << endl
-        << "  klick [ options ] -f tempomap-file" << endl
+        << "  klick [ options ] -f filename" << endl
         << "  klick [ options ] -j" << endl
         << endl
         << "Options:" << endl
-        <<  "  -f <filename>        load tempomap from file" << endl
-        <<  "  -j                   no tempomap, just follow jack transport" << endl
-        <<  "  -n <name>            jack client name" << endl
-        <<  "  -p <port,..>         jack port(s) to connect to" << endl
-        <<  "  -P                   automatically connect to hardware ports" << endl
-        <<  "  -s <number>          use built-in click sounds 1 (default), 2 or 3" << endl
-        <<  "  -S <emphasis,normal> use the given files as click sounds" << endl
-        <<  "  -e                   no emphasized beats" << endl
-        <<  "  -E                   emphasized beats only" << endl
-        <<  "  -v <multiplier>      adjust playback volume" << endl
-        <<  "  -w <multiplier>      adjust playback frequency" << endl
-        <<  "  -t                   enable jack transport" << endl
-        <<  "  -T                   become transport master (implies -t)" << endl
-        <<  "  -d <seconds>         delay before starting playback" << endl
-        <<  "  -c <bars>            pre-roll. use -c 0 for 2 beats" << endl
-        <<  "  -l <label>           start playback at the given label" << endl
-        <<  "  -x <multiplier>      multiply tempo by the given factor" << endl
-        <<  "  -V                   verbose output" << endl
-        <<  "  -h                   show this help" << endl
+        <<  "  -f filename       load tempomap from file" << endl
+        <<  "  -j                no tempomap, just follow jack transport" << endl
+        <<  "  -n name           jack client name" << endl
+        <<  "  -p port,..        jack port(s) to connect to" << endl
+        <<  "  -P                automatically connect to hardware ports" << endl
+        <<  "  -s number         use built-in sounds 1 (sine), 2 (noise) or 3 (bell/click)" << endl
+        <<  "  -S file[,file]    load sounds from file(s)" << endl
+        <<  "  -e                no emphasized beats" << endl
+        <<  "  -E                emphasized beats only" << endl
+        <<  "  -v multiplier     adjust playback volume" << endl
+        <<  "  -w multiplier     adjust playback frequency" << endl
+        <<  "  -t                enable jack transport" << endl
+        <<  "  -T                become transport master (implies -t)" << endl
+        <<  "  -d seconds        delay before starting playback" << endl
+        <<  "  -c bars           pre-roll. use -c 0 for 2 beats" << endl
+        <<  "  -l label          start playback at the given label" << endl
+        <<  "  -x multiplier     multiply tempo by the given factor" << endl
+        <<  "  -V                verbose output" << endl
+        <<  "  -h                show this help" << endl
         << endl
         << "Tempomap file format:" << endl
         << "  [label:] bars [meter] tempo[-tempo2] [pattern] [volume]" << endl
@@ -249,15 +249,19 @@ void Klick::Options::parse(int argc, char *argv[])
                 if (!settings.empty()) settings += " ";
                 settings += string(::optarg);
                 break;
+
             case 'f':
                 filename = string(::optarg);
                 break;
+
             case 'j':
                 follow_transport = true;
                 break;
+
             case 'n':
                 client_name = string(::optarg);
                 break;
+
             case 'p':
               { char_sep sep(",");
                 tokenizer tok(string(::optarg), sep);
@@ -265,71 +269,93 @@ void Klick::Options::parse(int argc, char *argv[])
                     connect_ports.push_back(*i);
                 }
               } break;
+
             case 'P':
                 auto_connect = true;
                 break;
+
             case 's':
                 click_sample = strtoul(::optarg, &end, 10);
                 if (*end != '\0' || click_sample < 1 || click_sample > 3) {
                     throw "invalid click sample number";
                 }
                 break;
+
             case 'S':
               { char_sep sep(",");
                 string str(::optarg);
                 tokenizer tok(str, sep);
-                if (count_iter(tok) != 2) {
-                    throw "wrong number of sample file names";
-                }
                 tokenizer::iterator i = tok.begin();
                 click_filename_emphasis = *i++;
-                click_filename_normal = *i++;
+                switch (count_iter(tok)) {
+                  case 1:
+                    click_filename_normal = click_filename_emphasis;
+                    break;
+                  case 2:
+                    click_filename_normal = *i++;
+                    break;
+                  default:
+                    throw "too many sample file names";
+                }
                 click_sample = CLICK_SAMPLE_FROM_FILE;
               } break;
+
             case 'e':
                 emphasis = EMPHASIS_NONE;
                 break;
+
             case 'E':
                 emphasis = EMPHASIS_ALL;
                 break;
+
             case 'v':
                 volume = strtof(::optarg, &end);
                 if (*end != '\0') throw "invalid volume";
                 break;
+
             case 'w':
                 frequency = strtof(::optarg, &end);
                 if (*end != '\0') throw "invalid frequency multiplier";
                 break;
+
             case 't':
                 transport_enabled = true;
                 break;
+
             case 'T':
                 transport_master = true;
                 transport_enabled = true;
                 break;
+
             case 'd':
                 delay = strtod(::optarg, &end);
                 if (*end != '\0' || delay < 0.0f) throw "invalid delay";
                 break;
+
             case 'c':
                 preroll = strtoul(::optarg, &end, 10);
                 if (*end != '\0') throw "invalid pre-roll";
                 break;
+
             case 'l':
                 start_label = string(::optarg);
                 break;
+
             case 'x':
                 tempo_multiplier = strtof(::optarg, &end);
                 if (*end != '\0') throw "invalid tempo multiplier";
                 break;
+
             case 'V':
                 verbose = true;
                 break;
+
             case 'h':
                 print_version(cout);
                 print_usage(cout);
                 exit(EXIT_SUCCESS);
                 break;
+
             default:
                 print_usage(cerr);
                 exit(EXIT_FAILURE);

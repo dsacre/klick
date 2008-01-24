8f771befaff9f6165de6c675ff60fbcf76462bdb
Dominic Sacré <dominic.sacre@gmx.de>
2008-01-24 23:13:45 +0000

    MetronomeJack code cleanup, added asserts
    git-svn-id: svn+ssh://kobol/srv/svn/klick/trunk@106 c380f6eb-e629-0410-afc2-bdd98b771d42

--- metronome_jack.cc
+++ metronome_jack.cc
@@ -43,9 +43,17 @@ void MetronomeJack::process_callback(sample_t *buffer, nframes_t nframes)
         return;
     }
 
+    // make sure the transport master provided us with sane position info
+    ASSERT(pos.beats_per_minute > 0.0);
+    ASSERT(pos.beats_per_bar > 0.0);
+    ASSERT(pos.ticks_per_beat > 0.0);
+    ASSERT(pos.beat > 0 && pos.beat <= pos.beats_per_bar);
+    ASSERT(pos.tick >= 0 && pos.tick < pos.ticks_per_beat);
+
     // convert BBT position to a frame number in this period
     double frames_per_beat = Audio->samplerate() * 60.0 / pos.beats_per_minute;
     nframes_t offset = (nframes_t)(frames_per_beat * (1.0 - (pos.tick / pos.ticks_per_beat)));
+    bool emphasis;
 
     // avoid playing the same click twice due to rounding errors
     if (_last_click_frame && (pos.frame >= _last_click_frame) &&
@@ -55,20 +63,30 @@ void MetronomeJack::process_callback(sample_t *buffer, nframes_t nframes)
 
     if (offset % (nframes_t)frames_per_beat == 0) {
         // click starts at first frame. pos already refers to this beat
-        bool emphasis = (pos.beat == 1);
+        offset = 0;
+        emphasis = (pos.beat == 1);
+
         // deal with faulty timebase masters
         if ((pos.beat == pos.beats_per_bar + 1) ||
             (pos.beat == pos.beats_per_bar && pos.tick == pos.ticks_per_beat)) {
             emphasis = true;
         }
-        play_click(emphasis, 0);
+
         _last_click_frame = pos.frame;
     }
     else if (offset < nframes) {
         // click starts somewhere during this period. since pos is the position at the start
         // of the period, the click played is actually at "pos + 1"
-        bool emphasis = (pos.beat == (int)pos.beats_per_bar);
-        play_click(emphasis, offset);
+        emphasis = (pos.beat == (int)pos.beats_per_bar);
+
         _last_click_frame = pos.frame + offset;
     }
+    else {
+        // no click in this period
+        return;
+    }
+
+    //std::cout << (pos.frame + offset) << ": " << emphasis << std::endl;
+
+    play_click(emphasis, offset);
 }
